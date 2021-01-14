b8e8617ea52328e778696897abce5658b099e537
Nevan Padayachee <padayacheenevan067@gmail.com>
2021-01-14 15:27:52 +0200

    part2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)#instance of flask webapp
 
@@ -6,15 +6,16 @@ a=False
 
 @app.route("/") #when app is run it will run idex
 def index():
-    return "<h1> hello world </h1>"
+    return render_template("index.html", content=["nev", "kev", "steve"])
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+#@app.route("/<name>")
+#def user(name):
+#    return f"Hello {name}!"
 
-@app.route("/admin")
-def admin():
-    if a:
-        return redirect(url_for("index"))
+#@app.route("/admin/")
+#def admin():
+#    if a:
+#        return redirect(url_for("index"))
+
 if __name__=="__main__":
     app.run()
