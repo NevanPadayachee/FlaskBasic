3501a6f0f02d0fdda7cf603e94bbe3e7acd048ec
Nevan Padayachee <padayacheenevan067@gmail.com>
2021-01-15 14:18:13 +0200

    added post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for,\
+    render_template, request
 
 app = Flask(__name__)#instance of flask webapp
 
@@ -16,6 +17,18 @@ def index():
 #def admin():
 #    if a:
 #        return redirect(url_for("index"))
+@app.route("/login", methods =["POST","GET"])
+def login():
+    if request.method == "POST":
+        user=request.form["nm"]
+        return redirect(url_for("user", usr=user))
+
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True )
